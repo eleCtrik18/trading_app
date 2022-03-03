caaa4233a749ac1ab44fb6640a7a68e1dfdfa5e5
Chetan Singh <54989039+eleCtrik18@users.noreply.github.com>
2022-03-03 11:01:26 +0530

    Update appBar.dart

--- lib/Components/appBar.dart
+++ lib/Components/appBar.dart
@@ -1,1 +1,43 @@
+//Custom Appbar
+//Using Preferred Size to Make Custom designed AppBar
+//This widget does not impose any constraints on its child, and it doesn't affect the child's layout in any way. It just advertises a preferred size which can be used by the parent.
 
+
+appBar: PreferredSize(
+          preferredSize: Size(double.infinity, kToolbarHeight),
+          child: ClipRRect(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+            child: AppBar(
+              brightness: Brightness.light,
+              backgroundColor: Colors.green.withOpacity(.5),
+              elevation: 0,
+              title: Text(
+                ' Trading App',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 17,
+                  color: Colors.black.withOpacity(.7),
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                  tooltip: 'User',
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.orange,
+                  icon: Icon(Icons.person, color: Colors.black.withOpacity(.7)),
+                  onPressed: () {
+                    HapticFeedback.lightImpact();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) {
+                        return UserInfo();
+                      }),
+                    );
+                  },
+                ),
+                Text('  '),
+              ],
+            ),
+          ),
+        ),
