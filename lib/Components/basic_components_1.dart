519e9f6e1728f8c7054e828a1b0b4249af02a473
Chetan Singh <54989039+eleCtrik18@users.noreply.github.com>
2022-03-04 20:11:46 +0530

    Create basic_components_1.dart

---
+++ lib/Components/basic_components_1.dart
@@ -0,0 +1,37 @@
+//Circular widget to display grid
+
+header1() {
+    double _w = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, _w / 10, _w / 20, 0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            height: _w / 8.5,
+            width: _w / 8.5,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(.1),
+                  blurRadius: 30,
+                  offset: Offset(0, 15),
+                ),
+              ],
+              shape: BoxShape.circle,
+            ),
+            child: IconButton(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              tooltip: 'Settings',
+              icon: Icon(Icons.text_fields,
+                  size: _w / 17, color: Colors.black.withOpacity(.6)),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
